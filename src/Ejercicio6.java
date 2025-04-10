ca42b00345a65209d371b693e560682454f1bbe4
Pablo <pablodmconti@gmail.com>
2025-04-10 10:39:41 -0300

    Segundo commit

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,5 +1,11 @@
 //Ejercicio 6 - Suma de números del 1 al 100 (for)
-//Importar librería
-import java.util.Scanner;
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            suma = suma + i;
+        }
+        System.out.println("La suma de los números del 1 al 100 es: "+suma);
+    }
 }
--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -1,5 +1,14 @@
 //Ejercicio 7 - Mostrar números pares del 1 al 50 (while)
-//Importar librería
-import java.util.Scanner;
 public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 50){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        i++;
+        }
+    }
 }
